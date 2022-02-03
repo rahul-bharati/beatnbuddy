4f57073325de0597ed45d91cec16bb3fd6a833f6
Rahul Bharati <rahulrbharati@gmail.com>
2022-02-03 19:00:52 +0530

    Adding Navigation bar

---
+++ client/components/Navbar.tsx
@@ -0,0 +1,30 @@
+import type { NextComponentType } from "next";
+import Link from "next/link";
+
+const Navbar: NextComponentType = () => {
+  return (
+    <nav className="bg-transparent py-2 px-4 fixed t-0 l-0 w-screen z-10">
+      <div className="max-w-[1200px] w-100 mx-auto flex items-center justify-between">
+        <Link href="/" passHref>
+          <h3 className="text-xl font-bold tracking-wide text-white">
+            BEAT&amp;BUDDY
+          </h3>
+        </Link>
+        <ul className="list-none flex items-center">
+          <li className="md:text-lg mr-4">
+            <button className="btn px-5 py-1 rounded-lg border-2 text-white">
+              Sign In
+            </button>
+          </li>
+          <li className="md:text-lg hidden sm:block">
+            <button className="btn px-5 py-1 rounded-lg text-white border-2 border-green-600 bg-green-600">
+              Create Account
+            </button>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,10 +1,12 @@
 import type { NextPage } from "next";
 import Header from "../components/Header";
+import Navbar from "../components/Navbar";
 import Player from "../components/Player";
 
 const Home: NextPage = () => {
   return (
     <>
+      <Navbar />
       <Header />
       <h1 className="text-3xl font-bold underline">Beat &amp; Buddy</h1>
       <Player />
